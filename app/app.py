f0019f46e3782d72062c166fb3d8d9e1ef8a0b84
Fl1nixxx <139043998+Fl1nixxx@users.noreply.github.com>
2026-06-14 20:22:57 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,11 +5,8 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import numpy as np
-import math
 
 import urllib.request
-import rasterio
-from rasterio.io import MemoryFile
 
 import streamlit as st
 
@@ -20,6 +17,8 @@ import torch.nn as nn
 from torchvision import transforms
 
 from model import build_model
+from count_area import count_building_area
+from count_area import define_pixel_area
 
 WEIGHTS_PATH = "best_weights.pth"
 WEIGHTS_URL = "https://github.com/Fl1nixxx/DLS-project/releases/download/v1.5/best_weights.pth"
@@ -50,30 +49,6 @@ def load_segmentation_model():
     model.load_state_dict(state_dict)
     model.eval()
     return model, device
-
-
-def define_pixel_area(uploaded_file):
-    try:
-        uploaded_file.seek(0)
-        with MemoryFile(uploaded_file.read()) as memfile:
-            with memfile.open() as src:
-                crs = src.crs
-                x, y = src.res
-                
-                if src.bounds.left == 0.0 and src.bounds.bottom == 0.0:
-                    return None 
-                
-                if crs and crs.is_projected:
-                    pixel_area = x * y
-                else:
-                    lat = src.bounds.bottom + (src.bounds.top - src.bounds.bottom) / 2
-                    meters_per_degree_lat = 111132
-                    meters_per_degree_lon = meters_per_degree_lat * math.cos(math.radians(lat))
-                    pixel_area = (x * meters_per_degree_lon) * (y * meters_per_degree_lat)
-                    
-        return pixel_area
-    except Exception:
-        return None
 
 def read_image(uploaded_file):
     image = Image.open(uploaded_file)
@@ -151,33 +126,6 @@ def make_mask_image(mask):
     mask_img = Image.fromarray((mask * 255).astype(np.uint8))
     return mask_img
 
-def count_building_area(mask, pixel_area, noise_threshold=10):
-    binary_mask = np.array(mask)
-    if binary_mask.max() == 1:
-        binary_mask = (binary_mask * 255).astype(np.uint8)
-    else:
-        binary_mask = (binary_mask > 127).astype(np.uint8) * 255
-
-    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    buildings_report = []
-    noise_count = 0
-
-    for cnt in contours:
-        pixel_count = cv2.contourArea(cnt)
-        area_sqm = pixel_count * pixel_area
-
-        if area_sqm < noise_threshold:
-            noise_count += 1
-            continue
-
-        area_rounded = round(area_sqm, 2)
-        buildings_report.append(area_rounded)
-        
-    total_area = sum(buildings_report)
-    buildings_count = len(buildings_report)
-    
-    return total_area, buildings_count, noise_count
 
 def image_to_png_bytes(image):
     buffer = io.BytesIO()
