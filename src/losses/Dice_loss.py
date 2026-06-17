4c5b3cddeb7ae4db552e11ce9d3c41f805fa7e41
Fl1nixxx <139043998+Fl1nixxx@users.noreply.github.com>
2026-06-17 23:31:57 +0300

    Update Dice_loss.py

--- src/losses/Dice_loss.py
+++ src/losses/Dice_loss.py
@@ -1,1 +1,16 @@
+import torch 
+import torch.nn as nn
 
+class DiceLoss(nn.Module):
+    def __init__(self, eps=1e-7):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, probs, masks):
+        masks = masks.float()
+
+        intersection = (probs * masks).sum(dim=(1, 2, 3))
+        union = probs.sum(dim=(1, 2, 3)) + masks.sum(dim=(1, 2, 3))
+
+        dice = (2 * intersection + self.eps) / (union + self.eps)
+        return 1 - dice.mean()
